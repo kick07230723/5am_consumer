3e9a2333bd267108639a1ec88dd5d4729f8d4ccb
choi-seunggyu <choi-seunggyu@DESKTOP-JHHKK9J>
2017-06-30 16:31:57 +0900

    ss

---
+++ 5am_consumer/src/main/java/org/cg/persistence/StoreDAOImpl.java
@@ -0,0 +1,18 @@
+package org.cg.persistence;
+
+import java.util.List;
+
+import org.cg.domain.StoreVO;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StoreDAOImpl implements StoreDAO {
+
+	@Override
+	public List<StoreVO> getlist() {
+		
+		return null;
+	}
+
+	
+}
